083dc3ae010c80572d5fdaff3d5af88f224f285d
Caleb-Andreas <caleb.andreas@mths.ca>
2024-11-07 11:12:46 -0500

    Made radio code

--- main.ts
+++ main.ts
@@ -1,8 +1,35 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Caleb Andreas
+ * Created on: Nov 2024
+ * This program tells you if you are too close using sonar
+ * on one microBit and sending a string to another microBit.
 */
 
-basic.showString('Hello, World!')
+// Variables.
+let distanceToObject: number = 0
+
+// Set group, cleanup then happy face at start
+radio.setGroup(149)
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// When a button pressed check distanceToObject and send string.
+input.onButtonPressed(Button.A, function() {
+    distanceToObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+    if (distanceToObject < 10) {
+        radio.sendString("Too Close")
+    } else {
+        radio.sendString("Good")
+    }
+})
+
+radio.onReceivedString(function (receivedString) {
+    basic.clearScreen()
+    basic.showString(receivedString)
+    basic.showIcon(IconNames.Happy)
+})
