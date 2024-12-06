c1439bfac5dd8edd0999ccd0c8f40c0bbd5a8871
Caleb-Andreas <caleb.andreas@mths.ca>
2024-12-06 16:26:08 +0000

    started code

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,75 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Caleb Andreas
+Created on: Dec 2024
+This module is a Micro:bit MicroPython program that tells you if you are too close using sonar
+on one microBit and sending a string to another microBit.
 """
 
 from microbit import *
+import * from radio
 
 
-display.scroll("Hello, World!")
+class HCSR04:
+    # This class abstracts out the functionality of the HC-SR04 and
+    # returns distance in mm.
+    def __init__(self, tpin=pin1, epin=pin2, spin=pin13):
+        self.trigger_pin = tpin
+        self.echo_pin = epin
+        self.sclk_pin = spin
+
+    def distance_mm(self):
+        spi.init(
+            baudrate=125000,
+            sclk=self.sclk_pin,
+            mosi=self.trigger_pin,
+            miso=self.echo_pin,
+        )
+        pre = 0
+        post = 0
+        k = -1
+        length = 500
+        resp = bytearray(length)
+        resp[0] = 0xFF
+        spi.write_readinto(resp, resp)
+        # find first non zero value
+        try:
+            i, value = next((ind, v) for ind, v in enumerate(resp) if v)
+        except StopIteration:
+            i = -1
+        if i > 0:
+            pre = bin(value).count("1")
+            # find first non full high value afterwards
+            try:
+                k, value = next(
+                    (ind, v)
+                    for ind, v in enumerate(resp[i : length - 2])
+                    if resp[i + ind + 1] == 0
+                )
+                post = bin(value).count("1") if k else 0
+                k = k + i
+            except StopIteration:
+                i = -1
+        dist = -1 if i < 0 else round(((pre + (k - i) * 8.0 + post) * 8 * 0.172) / 2)
+        return dist
+
+
+# Variables.
+sonar = HCSR04()
+radio.setGroup(149)
+distance_To_Object = 1
+
+# Clean up.
+display.clear()
+display.show(Image.HAPPY)
+
+# Find distance on a button press.
+while True:
+    if button_a.is_pressed():
+        display.clear()
+        distance_To_Object = sonar.distance_mm()
+        display.show(int(sonar.distance_mm() / 10))
+        radio.send(int(sonar.distance_mm() / 10))
+        sleep(1000)
+        display.clear()
+        display.show(Image.HAPPY)
+
